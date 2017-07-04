478b0c518baaf5f7e78d28ddd0ec34f73441266d
Tadeusz Andrzej Kadłubowski <tkadlubo@gmail.com>
2017-07-04 20:45:38 +0200

    Figure out current time.

--- pam-moontool.c
+++ pam-moontool.c
@@ -1,11 +1,19 @@
-  /* Define which PAM interfaces we provide */
-  #define PAM_SM_ACCOUNT
+/* Define which PAM interfaces we provide */
+#define PAM_SM_ACCOUNT
 
-  /* Include PAM headers */
-  #include <security/pam_appl.h>
-  #include <security/pam_modules.h>
+/* Include PAM headers */
+#include <security/pam_appl.h>
+#include <security/pam_modules.h>
 
-  /* PAM entry point for accounting */
-  int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-          return(PAM_IGNORE);
-  }
+#include <time.h>
+
+/* PAM entry point for accounting */
+int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+	double cur_phase;
+
+	time_t cur_time;
+	time(&cur_time);
+	struct tm* cur_gmt_time = gmtime(&cur_time);
+
+	return(PAM_IGNORE);
+}
